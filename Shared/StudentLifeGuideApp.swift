506420fd966567c96b4d5b615d448bebb02e0471
Chen Sihan <schen@lcs.on.ca>
2021-01-26 08:38:04 -0500

    Initial Commit

---
+++ Shared/StudentLifeGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentLifeGuideApp.swift
+//  Shared
+//
+//  Created by Chen, Sihan on 2021-01-26.
+//
+
+import SwiftUI
+
+@main
+struct StudentLifeGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
