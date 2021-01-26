c8ebf35dacc10a5d88c7ab269bcb820327288775
Chen Sihan <schen@lcs.on.ca>
2021-01-26 08:59:40 -0500

    basic layout of the UI and the test data

--- Shared/Model/LifeGuide.swift
+++ Shared/Model/LifeGuide.swift
@@ -15,3 +15,16 @@ struct LifeGuide: Identifiable {
     let imageName: String
     
 }
+
+let testData = [LifeGuide(name: "test 1", description: "This is test 1 content", warning: "1 is dangerous", imageName: "system"),
+                LifeGuide(name: "test 2", description: "This is test 2 content", warning: "2 is dangerous", imageName: "system2"),
+                LifeGuide(name: "test 3", description: "This is test 3 content", warning: "3 is dangerous", imageName: "system3"),
+                LifeGuide(name: "test 4", description: "This is test 4 content", warning: "4 is dangerous", imageName: "system4"),
+                LifeGuide(name: "test 5", description: "This is test 5 content", warning: "5 is dangerous", imageName: "system5")
+
+
+
+
+
+
+]
--- Shared/View/LifeGuideDetailView.swift
+++ Shared/View/LifeGuideDetailView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct LifeGuideDetailView: View {
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            
+            
+            HStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                    
+                Spacer()
+                
+            }.navigationTitle("Student Life Guide")
+            
+            
+        }
+        
+            
     }
+    
+    
+    
 }
 
 struct LifeGuideDetailView_Previews: PreviewProvider {
