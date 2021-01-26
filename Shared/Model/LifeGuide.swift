06b8dd8c90ad1bd89a6b627fefd21ab6354adc08
Chen Sihan <schen@lcs.on.ca>
2021-01-26 08:46:57 -0500

    add model folder

---
+++ Shared/Model/LifeGuide.swift
@@ -0,0 +1,17 @@
+//
+//  LifeGuide.swift
+//  StudentLifeGuide
+//
+//  Created by Chen, Sihan on 2021-01-26.
+//
+
+import Foundation
+
+struct LifeGuide: Identifiable {
+    let id = UUID()
+    let name: String
+    let description: String
+    let warning: String
+    let imageName: String
+    
+}
